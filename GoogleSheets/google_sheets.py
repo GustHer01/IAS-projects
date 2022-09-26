e548972b7bdfe77811ddd2fa1f1889953d7f36bd
ghernandez-ias <ghernandez@integralads.com>
2022-09-26 14:11:21 -0500

    Minor functionality changes

--- GoogleSheets/google_sheets.py
+++ GoogleSheets/google_sheets.py
@@ -19,7 +19,9 @@ class GoogleSheet():
         try:
             worksheet = self.sh.worksheet_by_title(worksheet_name)
         except pygsheets.WorksheetNotFound:
+            print('Creating worksheet')
             worksheet = self.sh.add_worksheet(worksheet_name)
+            print('worksheet created')
         
         return worksheet
     
@@ -31,60 +33,88 @@ class GoogleSheet():
     def join_data(self, data, df, today):
         # calculating dod, mom and wow
         # joining new calculations dataframe with old dataframe from the worksheet
-        values = data.values.tolist()
+        if df.values.tolist() == []:
 
-        date_ev = today - timedelta(days = 1)
-        df_old = df.loc[df['Date'] == date_ev.strftime("%Y-%m-%d")]
-        day = df_old.values.tolist()
+            df_new = pd.concat([df, data], ignore_index=True,sort = False).fillna(' ')
+            return df_new, data.values.tolist()
+        
+        else:
 
-        week_ev = today - timedelta(days = 7)
-        df_old = df.loc[df['Date'] == week_ev.strftime("%Y-%m-%d")]
-        week = df_old.values.tolist()
+            values = data.values.tolist()
 
-        month_ev = today - timedelta(days = 31)
-        df_old = df.loc[df['Date'] == month_ev.strftime("%Y-%m-%d")]
-        month = df_old.values.tolist()
+            date_ev = today - timedelta(days = 1)
+            df_old = df.loc[df['Date'] == date_ev.strftime("%Y-%m-%d")]
+            day = df_old.values.tolist()
 
-        for x in range(len(day)):
-            for y in range(len(day[x])):
-                if day:
-                    if ',' in str(day[x][y]):
-                        day[x][y] = day[x][y].replace(',','')
+            week_ev = today - timedelta(days = 7)
+            df_old = df.loc[df['Date'] == week_ev.strftime("%Y-%m-%d")]
+            week = df_old.values.tolist()
 
-                if week:
-                    if ',' in str(week[x][y]):
-                        week[x][y] = week[x][y].replace(',','')
-        
-                if month:   
-                    if ',' in str(month[x][y]):
-                        month[x][y] = month[x][y].replace(',','')
+            month_ev = today - timedelta(days = 31)
+            df_old = df.loc[df['Date'] == month_ev.strftime("%Y-%m-%d")]
+            month = df_old.values.tolist()
 
-        for i in range(0,len(values)):
-            if int(values[i][2]) == 0:
-                day_athena, week_athena, month_athena = 0,0,0
-        
-            else:
-                day_athena = round(((int(values[i][2]) - int(day[i][2]))/int(values[i][2])) * 100,3)
-                week_athena = round(((int(values[i][2]) - int(week[i][2]))/int(values[i][2])) * 100,3)
-                month_athena = round(((int(values[i][2]) - int(month[i][2]))/int(values[i][2])) * 100,3)
-        
-            if int(values[i][3]) == 0:
-                day_snow,week_snow,month_snow = 0,0,0
-        
-            else:
-                day_snow = round(((int(values[i][3]) - int(day[i][3]))/int(values[i][3])) * 100,3)
-                week_snow = round(((int(values[i][3]) - int(week[i][3]))/int(values[i][3])) * 100,3)
-                month_snow = round(((int(values[i][3]) - int(month[i][3]))/int(values[i][3])) * 100,3)
-        
-            values[i].extend([week_athena, week_snow, day_athena, day_snow, month_athena, month_snow])
-        
-        df_calc = pd.DataFrame(values, columns = ['Date', 'Partner','Partner_raw','Snowflake','%_diff_between_Snowflake_and_partner_raw',
-                                          '%_diff_week_athena','%_diff_week_snowflake','%_diff_day_athena','%_diff_day_snowflake',
-                                         '%_diff_month_athena','%_diff_month_snowflake'])
-        df_new = pd.concat([df, df_calc], ignore_index=True,sort = False).fillna(' ')
-        
-        return df_new, values
+            for x in range(len(day)):
+                for y in range(len(day[x])):
+                    if day:
+                        if ',' in str(day[x][y]):
+                            day[x][y] = day[x][y].replace(',','')
 
+                        if week:
+                            if ',' in str(week[x][y]):
+                                week[x][y] = week[x][y].replace(',','')
+    
+                            if month:   
+                                if ',' in str(month[x][y]):
+                                    month[x][y] = month[x][y].replace(',','')
+
+            for i in range(0,len(values)):
+                if int(values[i][2]) == 0:
+                    day_athena, week_athena, month_athena = 0,0,0
+    
+                else:
+                    if day:
+                        day_athena = round(((int(values[i][2]) - int(day[i][2]))/int(values[i][2])) * 100,3)
+
+                        if week:
+                            week_athena = round(((int(values[i][2]) - int(week[i][2]))/int(values[i][2])) * 100,3)
+                
+                            if month:
+                                month_athena = round(((int(values[i][2]) - int(month[i][2]))/int(values[i][2])) * 100,3)
+                            else:
+                                month_athena = 0
+                        else:
+                            week_athena = 0
+                            month_athena = 0
+                    else:
+                        day_athena = 0
+    
+                if int(values[i][3]) == 0:
+                    day_snow,week_snow,month_snow = 0,0,0
+    
+                else:
+                    if day:
+                        day_snow = round(((int(values[i][3]) - int(day[i][3]))/int(values[i][3])) * 100,3)
+                        if week:
+                            week_snow = round(((int(values[i][3]) - int(week[i][3]))/int(values[i][3])) * 100,3)
+                            if month:
+                                month_snow = round(((int(values[i][3]) - int(month[i][3]))/int(values[i][3])) * 100,3)
+                            else:
+                                month_snow = 0
+                        else:
+                            week_snow = 0
+                            month_snow = 0
+                    else:
+                        day_snow = 0
+    
+                values[i].extend([week_athena, week_snow, day_athena, day_snow, month_athena, month_snow])
+    
+            df_calc = pd.DataFrame(values, columns = ['Date', 'Partner','Partner_raw','Snowflake','%_diff_between_Snowflake_and_partner_raw',
+                                            '%_diff_week_athena','%_diff_week_snowflake','%_diff_day_athena','%_diff_day_snowflake',
+                                            '%_diff_month_athena','%_diff_month_snowflake'])
+            df_new = pd.concat([df, df_calc], ignore_index=True,sort = False).fillna(' ')
+    
+            return df_new, values
     
     def update_sheet(self, df, worksheet):
         # updating the worksheet with new dataframe
--- tools/calculations.py
+++ tools/calculations.py
@@ -12,17 +12,22 @@ def check_drops(data,values):
 
 # dividing data in days, weeks and months to check for drops
 def filter_values(values,email_day,new_alert):
-    if values[0][5] and values[0][6]:
-        week_drops = [x[5:7] for x in values]
-        week_d = check_drops(week_drops, values)
+    week_d, day_d, month_d = [],[],[]
+    week_string, day_string, month_string = """""","""""",""""""
     
-    if values[0][7] and values[0][8]:
-        day_drops = [x[7:9] for x in values]
-        day_d = check_drops(day_drops, values)
+    if 5 < len(values[0]):
+        if values[0][5] != 0 and values[0][6] != 0:
+            week_drops = [x[5:7] for x in values]
+            week_d = check_drops(week_drops, values)
 
-    if values[0][9] and values[0][10]:
-        month_drops = [x[9:11] for x in values]
-        month_d = check_drops(month_drops, values)
+        if values[0][7] != 0 and values[0][8] != 0:
+            day_drops = [x[7:9] for x in values]
+            day_d = check_drops(day_drops, values)
+
+        if values[0][9] != 0 and values[0][10] != 0:
+            month_drops = [x[9:11] for x in values]
+            month_d = check_drops(month_drops, values)
+
     
     if week_d:
         week_string = f"""Date: {email_day}\nDrop higher than 10% detected for: {week_d}\nFor: week over week comparisons"""
@@ -41,3 +46,5 @@ def filter_values(values,email_day,new_alert):
         body = '\n \n \n'.join([body, details])
 
         new_alert.send_email(body,'PMI-ALERTS')
+
+    return
